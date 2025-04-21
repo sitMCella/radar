28fd9fcd19ea6c28b887a0c842cb34ceaa7acc0a
Marco Cella <phcella@libero.it>
2025-04-21 10:46:07 +0200

    Update load test

--- radardevice/load_test.sh
+++ radardevice/load_test.sh
@@ -4,7 +4,7 @@
 # - Install Cargo
 # - Build release the Radar Device application using Cargo
 
-for i in {1001..1002}
+for i in {1..1001}
 do
    ./target/release/radardevice --id "d$i" --latitude 50.34 --longitude 80.30 --radius 50 --file-name "dev1_signals.csv" >> load_test.log 2>&1 &
 done
