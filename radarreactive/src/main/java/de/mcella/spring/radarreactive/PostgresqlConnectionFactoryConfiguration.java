596b12421da159cf54188d0e588eddfb00951fa9
Marco Cella <phcella@libero.it>
2025-04-26 14:28:04 +0200

    Use environment variables in PostgresqlConnectionFactoryConfiguration

--- radarreactive/src/main/java/de/mcella/spring/radarreactive/PostgresqlConnectionFactoryConfiguration.java
+++ radarreactive/src/main/java/de/mcella/spring/radarreactive/PostgresqlConnectionFactoryConfiguration.java
@@ -2,20 +2,33 @@ package de.mcella.spring.radarreactive;
 
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PostgresqlConnectionFactoryConfiguration {
 
+  @Value("${DATABASE_HOST}")
+  private String databaseHost;
+
+  @Value("${DATABASE_NAME}")
+  private String databaseName;
+
+  @Value("${SPRING_R2DBC_USERNAME}")
+  private String databaseUsername;
+
+  @Value("${SPRING_R2DBC_PASSWORD}")
+  private String databasePassword;
+
   @Bean
   public PostgresqlConnectionFactory postgresqlConnectionFactory() {
     return new PostgresqlConnectionFactory(
         PostgresqlConnectionConfiguration.builder()
-            .host("db")
-            .database("radarhub")
-            .username("postgres")
-            .password("mysecretpassword")
+            .host(databaseHost)
+            .database(databaseName)
+            .username(databaseUsername)
+            .password(databasePassword)
             .build());
   }
 }
